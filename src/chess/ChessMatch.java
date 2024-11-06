921aa277ced03cac0054fd748f820ccc86ea13b8
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2024-11-06 16:16:31 -0300

    Peon

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,6 +6,7 @@ import boardGame.Position;
 import chess.Enums.Color;
 import chess.Exceptions.ChessException;
 import chess.pieces.King;
+import chess.pieces.Peon;
 import chess.pieces.Rook;
 
 import java.util.ArrayList;
@@ -192,12 +193,16 @@ public class ChessMatch {
     }
 
     public void initialSetup() {
-        placeNewPiece('h', 7, new Rook(board, Color.WHITE));
+       // placeNewPiece('h', 7, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
-        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('a',1, new Peon(board,Color.WHITE));
+        placeNewPiece('d',2, new Peon(board,Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 
-        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+
+        placeNewPiece('h', 2, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new King(board, Color.BLACK));
+        placeNewPiece('e',8, new Peon(board,Color.BLACK));
 
     }
 }
